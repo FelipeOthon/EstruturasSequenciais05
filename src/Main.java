a92634cb280dccb8bf8d5f3c63a929208e5316ec
FelipeOthon <felipeosantiago@gmail.com>
2022-10-14 23:14:34 -0300

    enviar readme

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+
+        Scanner sc =new Scanner(System.in);
+        int x;
+        System.out.println("Digite a medida em metros: ");
+        x = sc.nextInt();
+        System.out.println("a medida em centimetros e :" + (x*100 ));
     }
 }
